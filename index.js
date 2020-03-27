b8b6f3978e57c7117000fa51ddfaf97dad569f0d
moritzmitterdorfer <mitterdorfer.moritz@gmail.com>
2020-03-27 15:59:24 +0100

    imit2

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 7000;
 app.listen(port);
 
 console.log(`Password generator listening on ${port}`);
