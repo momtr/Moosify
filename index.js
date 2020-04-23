032fffef441dfedb8c2b8f07c4a5c291eea8c234
cien biswas <ryo.cien.biswas@gmail.com>
2020-04-23 13:59:49 +0200

    Authentication process

--- index.js
+++ index.js
@@ -1,34 +1,67 @@
+// Biswas, Maitz, Mitterdorfer
+// Server.js, 2020
+
+// Spotify 
+// move into env file!!
+
+const scopes = "user-read-private user-read-email user-read-recently-played";
+
+// npm modules
 const express = require('express');
 const path = require('path');
-const generatePassword = require('password-generator');
+const querystring = require('querystring');
+const fetch = require('node-fetch');
 
 const app = express();
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
-  const count = 5;
 
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
-
-  // Return them as json
-  res.json(passwords);
-
-  console.log(`Sent ${count} passwords`);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
+app.get('/redirect', (req, res) => {
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: 'code',
+        client_id: process.env.CLIENT_ID,
+        scope: scopes,
+        redirect_uri: "https://moosify.herokuapp.com/gotUser"
+      })
+  );
+}); 
+
+app.get('/gotUser', (req, res) => {
+  let code = req.query.code || null;
+  if(!code) {
+    res.json({
+      status: 'ERROR',
+      message: 'code not specified'
+    });
+    return;
+  }
+  let apiResponse = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    body: {
+      code: code, //user id
+      redirect_uri: 'https://moosify.herokuapp.com/gotUser',
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization' : 'Basic ' + new Buffer(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')
+    }
+  });
+  console.log(apiResponse);
+});
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.send('Page not found');
 });
 
 const port = process.env.PORT || 7000;
 app.listen(port);
 
-console.log(`Password generator listening on ${port}`);
+console.log(`Moosify listening on ${port}`);
