18e88d9cb0e4a67c537ffb0aa6750cb6994cfaea
Moritz Mitterdorfer <moritzmitterdorfer@Moritzs-MBP.home>
2020-05-06 00:53:10 +0200

    mood

--- api/moodometer.js
+++ api/moodometer.js
@@ -12,20 +12,25 @@ const cookieParser = require('cookie-parser');
 
 const SpotifyAPI = require('../libs/spotify/spotify.class');
 
+const SentimentAnalysis = require('../libs/sentimentAnalysis/sentimentAnalysis');
+const analyzer = new SentimentAnalysis('../libs/sentimentAnalysis/AFINN-111.json');
+
 const getRouter = (db) => {
     const router = express.Router();
     router.use(cookieParser());
 
     /** path: /api/v1/recommendedSongs/accessToken */
     router.get('/recommendedSongs/:accessToken', async (req, res, next) => {
-        let accessToken = req.params.accessToken;
-        let moodString = req.params.q;
+        let accessToken = req.params.accessToken || 0;
+        let moodString = req.params.q || '';
         /** get the username */
         let userObject = await SpotifyAPI.getCurrentUserObject(accessToken);
         let userID = userObject.id;
         /** get the user's songs from the database */
         let usersTracks = await db.getData(`users/${userID}/audioFeatures`);
-        /**  */
+        /** do sentiment analysis with the query string */
+        let mood = await analyzer.getScore(moodString);
+        res.send(JSON.stringify({ mood }));
     });
 
     return router;
