8050c0761193255d4bafac3c631c32eb89ca44a0
Moritz Mitterdorfer <moritzmitterdorfer@Moritzs-MBP.home>
2020-05-05 23:39:01 +0200

    test

--- auth/index.js
+++ auth/index.js
@@ -85,13 +85,11 @@ router.get("/gotUser", (req, res, next) => {
             /** get user data */
             let userObject = await SpotifyAPI.getCurrentUserObject(access_token);
             /** get user's recently played songs + audio features */
-            /*
-            let recentlyPlayedTrackIDs = await SpotifyAPI.getRecentlyPlayed(access_token, 70);
-            console.log(recentlyPlayedTrackIDs)
+            let recentlyPlayedTrackIDs = await SpotifyAPI.getRecentlyPlayed(access_token, 50);
+            db.insertData('tracks', Date.now(), recentlyPlayedTrackIDs);
             let trackIDs = SpotifyAPI.getSongIDsFromTrackArray(recentlyPlayedTrackIDs);
             let audioFeatures = await SpotifyAPI.getAudioFeaturesOfTracks(access_token, trackIDs);
             let user = { userData, audioFeatures };
-            */
             db.insertData('users', userObject.id, userObject);
 
         } else {
