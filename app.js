e9b5d462f63e3c54f76e7137822b15ac79bf70df
Moritz Mitterdorfer <moritzmitterdorfer@Moritzs-MBP.home>
2020-05-06 00:19:34 +0200

    test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 
-const firebase = require('../libs/database/database');
+const firebase = require('./libs/database/database');
 const db = new firebase.FirebaseRealTime();
 
 const app = express();
