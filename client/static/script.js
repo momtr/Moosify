848d1ae4eec12b511cb1121a10a9540d8679965b
Moritz Mitterdorfer <moritzmitterdorfer@Moritzs-MBP.home>
2020-05-06 02:57:14 +0200

    fix

--- client/static/script.js
+++ client/static/script.js
@@ -7,13 +7,15 @@ $(document).ready(function() {
         let inputString = $('#inputString').val();
 
         // get tracks from API
-        let accessToken = Cookies.get('accessToken');
-        let tracks = await fetch(`/api/v1/recommendedSongs/${accessToken}?q=${inputString}`); // we get 10 tracks by default
-        let json = await tracks.json();
+        let accessToken = Cookies.get('access_token');
+        let res = await fetch(`/api/v1/recommendedSongs/${accessToken}?q=${inputString}`); // we get 10 tracks by default
+        let json = await res.json();
 
         console.log(json);
 
-        for(let i of json) {
+        let tracks = json.data.tracks;
+
+        for(let i of tracks) {
             let imgURL = i.images[0].url;
             $('#tracks').append(`<div><img src="${imgURL}"><h3>${i.name}</h3></div>`)   
         }
