be159c3c4319b0a549d1bec775a990a15d16fff8
Moritz Mitterdorfer <moritzmitterdorfer@Moritzs-MBP.home>
2020-06-02 20:45:52 +0200

    frontend

--- api/index.js
+++ api/index.js
@@ -55,7 +55,7 @@ const getRouter = (db) => {
         /** user actions for that user (only 10 songs) */
         let ids = [];
         for(let i of trackFeatures) {
-            ids.push(i);
+            ids.push(i.id);
         }
         recon.userMultipleActions(userID, ids);
         /** get recommendations for user */      
